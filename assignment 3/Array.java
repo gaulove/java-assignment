d98576467f4e21febe1cd3e99e5378ef2d067788
Gaurav Neupane <45664904+gaulove@users.noreply.github.com>
2020-03-04 11:08:28 +0545

    Create Array.java

---
+++ assignment 3/Array.java
@@ -0,0 +1,9 @@
+ 
+class Array{
+    public static void main(String[] args){
+        String[]  names = {"Gaurav","Subash","Madan","Sanjeeb"};
+        for(int i = 0 ; i < names.length ; i++){
+            System.out.println(names[i]);
+        }
+    }
+}
