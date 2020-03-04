96a031b160c3b95c959fd9effd02036da15c50d4
Gaurav Neupane <45664904+gaulove@users.noreply.github.com>
2020-03-04 11:56:16 +0545

    Create NoLayout.java

---
+++ assignment 4/NoLayout.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class NoLayout  {
+
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("NoLayout Test");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setLayout(null);
+    JButton b1 = new JButton("b1");
+    b1.setBounds(20, 20, 100, 20);
+    JButton b2 = new JButton("b2");
+    b2.setBounds(124, 25, 100, 20);
+    frame.add(b1);
+    frame.add(b2);
+    frame.setSize(300, 100);
+    frame.setVisible(true);
+  }
+}
