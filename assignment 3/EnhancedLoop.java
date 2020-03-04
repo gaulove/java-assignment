7e95f45a0f3a3251cd6497682faab0fd63306568
Gaurav Neupane <45664904+gaulove@users.noreply.github.com>
2020-03-04 11:09:48 +0545

    Create EnhancedLoop.java

---
+++ assignment 3/EnhancedLoop.java
@@ -0,0 +1,9 @@
+class EnhancedLoop{
+    public static void main(String[] args){
+
+        char[] letters = {'a' , 'b' , 'c' , 'd' , 'e'};
+        for( char i: letters){
+            System.out.println(i);
+        }
+    }
+}
